84fb24f70a486e8318cb053b0a2cde9e0c0a8b37
Joaquín Campos <camposcolmenarejo.joaquin@gmail.com>
2016-08-04 20:45:39 +0200

    Fixed drawing system of board and implemented basic handler of rotary bevel

--- src/game.js
+++ src/game.js
@@ -10,30 +10,31 @@ var create = function() {
 	var game = this;
 	
 	game.stage.backgroundColor = '#FFFFFF';
-	
-	var NUM_ROWS = 10,
+
+	var NUM_ROWS = 8,
 		NUM_COLS = 12;
+	var DIAM_GUI = 75;	// The diameter of the gui. 
+
+	var SIZE_ROW = (game.world.width/2 - DIAM_GUI)/NUM_ROWS,
+		SIZE_COL = game.math.PI2/NUM_COLS;
 
 	// Create the structure
-	var board = new Array(NUM_ROWS);
-	for (var i = 0; i < NUM_ROWS; i++) {
-		board[i] = new Array(NUM_COLS);
-		for (var j = 0; j < NUM_COLS; j++) {
-			board[i][j] = null;
-		}
-	}
-
-	// Paint the board
-	var SIZE_ROW = (game.world.width/2)/NUM_ROWS;
-	var SIZE_COL = game.math.PI2/NUM_COLS;
-	for (var i = 0; i < NUM_COLS; i++) {
-		for (var j = NUM_ROWS; j > 0; j--) {
+	var i, j;
+	var board = [];
+	for (i = 0; i < NUM_ROWS; i++) {
+		board[i] = [];
+		for (j = 0; j < NUM_COLS; j++) {
 			var g = game.add.graphics(game.world.centerX, game.world.centerY);
 			g.lineStyle(SIZE_ROW, 0xFFFFFF, 1);
-			g.arc(0, 0, game.world.width/2 - j * (SIZE_ROW/2), SIZE_COL * i, SIZE_COL * (i + 1), false);
+			g.arc(0, 0, game.world.width/2 - (i*SIZE_ROW + (SIZE_ROW/2)), SIZE_COL * j, SIZE_COL * (j + 1), false);
 			g.tint = game.rnd.integer() * 0xFFFFFF;
+			board[i][j] = g;
 		}
 	}
+};
+
+function rotate(direction) {
+	console.log('Rotation ' + direction);
 };
 
 var update = function() {
