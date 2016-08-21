3ef74a45a871f90403c0f12503d6a56c49998ad4
Joaquín Campos <camposcolmenarejo.joaquin@gmail.com>
2016-08-21 18:43:19 +0200

    Refactor game structures; Add collision detection
    The game structures and many methods (certainly, almost all) were
    refactored just because the previous system causes great dependence
    between logical objects and graphics. The refactor had to be done in
    order to allow a simple implementation of side collision detection,
    committed here, too.
    
    Therefore, the major changes done are:
    
     - Blocks are stored in an array and pointed by a matrix that defines
       their positions in the game's board. Graphics are stored in a
       different matrix, too.
    
     - Painting/erasing methods were replaced by an easier graphics' tint
       attribute setter method that is automatically called by Phaser in the
       update function.
    
     - Rotation method was simplified and now just moves the elements along
       the matrix. It detects side collision between fixed blocks and
       falling blocks.

--- src/game.js
+++ src/game.js
@@ -11,7 +11,9 @@ var NUM_ROWS = 8,
 	NUM_COLS = 12;
 var DELAY_BLOCK_GEN = Phaser.Timer.SECOND * 2,
 	DELAY_BLOCK_FALL = Phaser.Timer.SECOND * 1;
-var board = [];
+var blocks = [],
+	board = [],
+	graphics = [];
 
 function generateBoard(game) {
 
@@ -21,18 +23,15 @@ function generateBoard(game) {
 	// Create the structure
 	for (let i = 0; i < NUM_ROWS; i++) {
 		board[i] = [];
+		graphics[i] = [];
 		for (let j = 0; j < NUM_COLS; j++) {
 
 			var g = game.add.graphics(game.world.centerX, game.world.centerY);
 			g.lineStyle(SIZE_ROW, 0xFFFFFF, 1);
 			g.arc(0, 0, game.world.width/2 - (i*SIZE_ROW + (SIZE_ROW/2)), SIZE_COL * j, SIZE_COL * (j + 1), false);
 
-			board[i][j] = {
-				graphic: g,
-				isActive: false,
-				isFalling: false,
-				tint: 0xFFFFFF
-			};
+			board[i][j] = null;
+			graphics[i][j] = g;
 		}
 	}
 };
@@ -46,112 +45,112 @@ var create = function() {
 	game.time.events.loop(DELAY_BLOCK_GEN, throwBlock, game);
 };
 
+function generateBlock(game, col) {
+	var tint = game.rnd.integer() * 0xFFFFFF; // Set the color of the block.
+
+	var block = {
+		row: NUM_ROWS - 1,
+		tint: tint,
+		isFalling: true
+	};
+	var position = blocks.push(block) - 1;
+	board[NUM_ROWS - 1][col] = blocks[position];
+	return blocks[position];
+};
+
 function throwBlock() {
 	var game = this;
 	
-	var tint = game.rnd.integer() * 0xFFFFFF; // Set the color of the block.
 	var column = game.rnd.integerInRange(0, NUM_COLS - 1); // Set the column where the block will fall.
 
 	// TODO: Prepare throw method (show to user where will the block be throw)
-	if (!board[NUM_ROWS - 1][column].isActive) { // Block can be thrown
-		game.time.events.add(DELAY_BLOCK_FALL, moveBlock, game, 0, column, tint);
+	if (!board[NUM_ROWS - 1][column] != null) { // Block can be thrown
+		var block = generateBlock(game, column);
+		game.time.events.add(DELAY_BLOCK_FALL, moveBlock, game, block);
 	}
 	else {
 		// TODO: Ends game (?)
 	}
 };
 
-function moveBlock(step, column, tint) {
+function findBlockColumn(block) {
+	var column;
+	for (let i = 0; i < NUM_ROWS; i++) {
+		column = board[i].indexOf(block);
+		if (column > -1) break;
+	}
+	return column;
+};
+
+function moveBlock(block) {
 	var game = this;
 
-	if (step != 0) { eraseBlock(NUM_ROWS - step, column); }
-	paintBlock(NUM_ROWS - step - 1, column, tint);
-	board[NUM_ROWS - step - 1][column].isFalling = true; // Mark it as a falling block.
+	var row = block.row;
+	var col = findBlockColumn(block);
 
-	if (step + 1 == NUM_ROWS) { 
-		board[NUM_ROWS - step - 1][column].isFalling = false; 
-		return; 
-	}
-	step++; // Make a step, that is, keep falling.
+	// The block has found another block in the next position.
+	if (board[row - 1][col] != null && !board[row -1][col].isFalling) {
+		block.isFalling = false;
+		return;
+	} 
 
-	if (!board[NUM_ROWS - step - 1][column].isActive) { // Block can be thrown
-		game.time.events.add(DELAY_BLOCK_FALL, moveBlock, game, step, column, tint);
+	block.row = row - 1;
+	board[row - 1][col] = block;
+	board[row][col] = null;
+
+	// The block has reached the last position in column.
+	if (row - 1 == 0) {
+		block.isFalling = false;
 	}
 	else {
-		board[NUM_ROWS - step][column].isFalling = false; // Mark it as a still block.
+		game.time.events.add(DELAY_BLOCK_FALL, moveBlock, game, block);
 	}
-};
-
-function paintBlock(row, col, tint) {
-	var block = board[row][col];
-	block.isActive = true;
-	block.tint = tint;
-	block.graphic.tint = tint;
-};
-
-function eraseBlock(row, col) {
-	if (col == -1) { col = NUM_COLS - 1; }
-	var block = board[row][col];
-	block.isActive = false;
-	block.isFalling = false;
-	block.tint = 0xFFFFFF; 
-	block.graphic.tint = 0xFFFFFF;
 };
 
 function rotate(direction) {
-	if (direction === 'CW') {
-		for (let i = 0; i < NUM_ROWS; i++) {
-			var block = board[i][NUM_COLS - 1];
-			var oldTint = block.tint,
-				allowMove = (block.isActive && !block.isFalling),
-				wasPainted = false;
-			for (let j = NUM_COLS - 1; j >= 0; j--) {
-				if (j == 0) {
-					if (allowMove) {
-						if (!wasPainted) { eraseBlock(i, -1); }
-						paintBlock(i, j, oldTint);
-					}
-					continue;
-				}
 
-				var nextBlock = board[i][j - 1];
-				if (nextBlock.isActive && !nextBlock.isFalling) {
-					if (j == NUM_COLS - 1) { wasPainted = true;}
-					paintBlock(i, j, nextBlock.tint);
-					eraseBlock(i, j - 1);
-				}
-			}
-		}
+	if (direction !== 'CW' && direction !== 'CCW') { 
+		return console.error('Only "CCW" and "CW" values are allowed'); 
 	}
-	else if (direction === 'CCW') {
-		for (let i = 0; i < NUM_ROWS; i++) {
-			var block = board[i][0];
-			var oldTint = block.tint,
-				allowMove = (block.isActive && !block.isFalling),
-				wasPainted = false;
-			for (let j = 0; j < NUM_COLS; j++) {
-				if (j == NUM_COLS - 1) {
-					if (allowMove) {
-						if (!wasPainted) { eraseBlock(i, 0); }
-						paintBlock(i, j, oldTint);
-					}
-					continue;
-				}
 
-				var nextBlock = board[i][j + 1];
-				if (nextBlock.isActive && !nextBlock.isFalling) {
-					if (j == 0) { wasPainted = true;}
-					paintBlock(i, j, nextBlock.tint);
-					eraseBlock(i, j + 1);
+	for (let i = 0; i < NUM_ROWS; i++) {
+		var fixedBlocks = board[i].map(function(block) {
+			return (block && block.isFalling ? null : block);
+		});
+		var fallingBlocks = board[i].map(function(block) {
+			return (block && !block.isFalling ? null : block); 
+		});
+
+		if (direction === 'CW') { fixedBlocks.unshift(fixedBlocks.pop()); }
+		else if (direction === 'CCW') { fixedBlocks.push(fixedBlocks.shift()); }
+
+		var resultRow = [];
+		for (let j = 0; j < fixedBlocks.length; j++) {
+			if (fallingBlocks[j] && fixedBlocks[j] ) { // Collision detected!
+				// WARNING: Possible collision between falling blocks in near future.
+				if (direction === 'CW') { 
+					if (j < NUM_COLS - 1) { fallingBlocks[j + 1] = fallingBlocks[j]; }
+					else { resultsRow[0] = fallingBlocks[j]; }
 				}
+				else { 
+					if (j > 0) { resultRow[j - 1] = fallingBlocks[j]; } 
+					else { fallingBlocks[fallingBlocks.length - 1] = fallingBlocks[j]; }
+				}
+				resultRow.push(fixedBlocks[j]);
+				continue;
 			}
+			else { resultRow.push(fallingBlocks[j] || fixedBlocks[j]); }	
 		}
+		board[i] = resultRow;
 	}
-	else { return console.error('Only "CCW" and "CW" values are allowed'); }
 };
 
 var update = function() {
-	
+	for (let i = 0; i < NUM_ROWS; i++) {
+		for (let j = 0; j < NUM_COLS; j++) {
+			graphics[i][j].tint = (board[i][j] != null ? board[i][j].tint : 0xFFFFFF);
+		}
+	}
 };
 
 Game.prototype = {
